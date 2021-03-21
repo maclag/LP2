d99dec659cca6c1e58592599f93a75ff59b3899e
Maria Clara Gomes <77252549+maclag@users.noreply.github.com>
2021-03-20 23:25:11 -0300

    Código

---
+++ 02-Classes/Exercício 3.2/PackApp.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import figures.*;
+
+class PackApp {
+    public static void main(String[] args) {
+        PackFrame frame = new PackFrame();
+        frame.setVisible(true);
+    }
+}
+
+class PackFrame extends JFrame {
+    Rect r1;
+    Ellipse e1;
+    Pentagon p1;
+
+    PackFrame () {
+        this.addWindowListener(
+                new WindowAdapter() {
+                    public void windowClosing (WindowEvent e) {
+                        System.exit(0);
+                    }
+                }
+        );
+        this.setTitle("Java Packages");
+        this.setSize(350, 350);
+
+        this.r1 = new Rect(50, 50, 100, 30, new Color(194, 161, 219), new Color(36, 161, 33));
+        this.e1 = new Ellipse(50, 100, 100, 30, new Color(201, 202, 134), Color.pink);
+
+        int[] xPoints = new int[]{140, 230, 195, 87, 50};
+        int[] yPoints = new int[]{150, 210, 300, 300, 210};
+        this.p1 = new Pentagon(xPoints, yPoints, 5, new Color(153, 212, 180), Color.blue);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        this.r1.paint(g);
+        this.e1.paint(g);
+        this.p1.paint(g);
+    }
+}
---
+++ 02-Classes/Exercício 3.2/figures/Pentagon.java
@@ -0,0 +1,33 @@
+package figures;
+
+import java.awt.*;
+
+public class Pentagon {
+    private int[] x, y;
+    private int n;
+    private Color background;
+    private Color outline;
+
+    public Pentagon(int[] x, int[] y, int n, Color background, Color outline) {
+        this.x = x;
+        this.y = y;
+        this.n = n;
+        this.background = background;
+        this.outline = outline;
+    }
+
+    public void print () {
+        System.out.printf("Poligono de %d lados com vértices em (%d, %d), (%d, %d), (%d, %d), (%d, %d) e (%d, %d).\n",
+                this.n, x[0], y[0], x[1], y[1], x[2], y[2], x[3], y[3], x[4], y[4]);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(this.background);
+        g2d.fillPolygon(this.x, this.y, this.n);
+
+        g2d.setColor(this.outline);
+        g2d.drawPolygon(this.x, this.y, this.n);
+    }
+}
