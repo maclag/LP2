76402b2d3c355dbd9ffb3a5a1f6dff30828a4804
Maria Clara Gomes <77252549+maclag@users.noreply.github.com>
2021-03-20 23:23:20 -0300

    Código

---
+++ 02-Classes/Exercício 3.1/figures/Pentagon.java
@@ -0,0 +1,24 @@
+package figures;
+
+import java.awt.*;
+
+public class Pentagon {
+    private int[] x, y;
+    private int n;
+
+    public Pentagon(int[] x, int[] y, int n) {
+        this.x = x;
+        this.y = y;
+        this.n = n;
+    }
+
+    public void print () {
+        System.out.printf("Poligono de %d lados com vértices em (%d, %d), (%d, %d), (%d, %d), (%d, %d) e (%d, %d).\n",
+                this.n, x[0], y[0], x[1], y[1], x[2], y[2], x[3], y[3], x[4], y[4]);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawPolygon(this.x, this.y, this.n);
+    }
+}
