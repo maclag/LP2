b77e2e4a800dbed4e902a81c9959fb90ebe75c52
Maria Clara Gomes <77252549+maclag@users.noreply.github.com>
2021-03-21 11:15:34 -0300

    Código

--- 02-Classes/Exercício 3.1/figures/Pentagon.java
+++ 02-Classes/Exercício 3.1/figures/Pentagon.java
@@ -13,8 +13,8 @@ public class Pentagon {
     }
 
     public void print () {
-        System.out.printf("Poligono de %d lados com vértices em (%d, %d), (%d, %d), (%d, %d), (%d, %d) e (%d, %d).\n",
-                this.n, x[0], y[0], x[1], y[1], x[2], y[2], x[3], y[3], x[4], y[4]);
+        System.out.printf("Pentágono de vértices em (%d, %d), (%d, %d), (%d, %d), (%d, %d) e (%d, %d).\n",
+                x[0], y[0], x[1], y[1], x[2], y[2], x[3], y[3], x[4], y[4]);
     }
 
     public void paint (Graphics g) {
