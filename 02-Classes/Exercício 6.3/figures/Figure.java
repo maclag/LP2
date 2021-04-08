0802d8565f270f571cd105a57a849282e098a563
Maria Clara Gomes <77252549+maclag@users.noreply.github.com>
2021-04-08 01:30:32 -0300

    Update Figure.java

--- 02-Classes/Exercício 6.3/figures/Figure.java
+++ 02-Classes/Exercício 6.3/figures/Figure.java
@@ -3,9 +3,9 @@ package figures;
 import java.awt.*;
 
 public abstract class Figure {
+    int x, y, w, h;
     Color background, outline;
 
     public abstract void print ();
-
     public abstract void paint (Graphics g);
 }
