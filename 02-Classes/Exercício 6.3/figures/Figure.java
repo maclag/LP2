871b727d8b6237e61730163e446f30a9f9ea36e0
Maria Clara Gomes <77252549+maclag@users.noreply.github.com>
2021-04-08 01:41:51 -0300

    Update Figure.java

--- 02-Classes/Exercício 6.3/figures/Figure.java
+++ 02-Classes/Exercício 6.3/figures/Figure.java
@@ -5,6 +5,15 @@ import java.awt.*;
 public abstract class Figure {
     int x, y, w, h;
     Color background, outline;
+    
+    public Figure (int x, int y, int w, int h, Color background, Color outline) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.background = background;
+        this.outline = outline;
+    }
 
     public abstract void print ();
     public abstract void paint (Graphics g);
