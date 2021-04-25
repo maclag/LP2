3b335f3422c724914bfcc9e38af1ba939cedd7b8
Maria Clara Gomes <77252549+maclag@users.noreply.github.com>
2021-04-25 18:02:57 -0300

    Color - background and border

---
+++ 03-Projeto-1-2/colors/Colors.java
@@ -0,0 +1,19 @@
+package colors;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Colors {
+    public static ArrayList<Color> colors = new ArrayList<>(10) {{
+        add(new Color(200, 112, 126));
+        add(new Color(237, 170, 125));
+        add(new Color(90, 160, 141));
+        add(new Color(168, 200, 121));
+        add(new Color(172, 153, 193));
+        add(new Color(150, 177, 208));
+        add(new Color(249, 205, 151));
+        add(new Color(192, 136, 99));
+        add(new Color(209, 141, 178));
+        add(new Color(112, 149, 225));
+    }};
+}
