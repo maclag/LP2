57553fac137fc1344a1586ed9fea87b023b4407d
Maria Clara Gomes <77252549+maclag@users.noreply.github.com>
2021-05-03 02:50:16 -0300

    Frame

--- 03-Projeto-1-2/ListFrame.java
+++ 03-Projeto-1-2/ListFrame.java
@@ -7,14 +7,19 @@ import figures.*;
 import colors.*;
 
 class ListFrame extends JFrame {
+    private static final long serialVersionUID = 1L;
+
     ArrayList<Figure> figs = new ArrayList<>();
     Figure focus = null;
     Rect rectFocus = null;
+    Rect rectResize = new Rect(0, 0, 15, 15, null, Color.red);
+    Point pos = null;
 
-    Point p = null;
-    Point beforePoint = null, afterPoint = null;
+    boolean pressedRes = false;
 
     int distanceX = 0, distanceY = 0;
+    int newDistanceX = 0, newDistanceY = 0;
+    int changeX = 0, changeY = 0;
     int iFilled = 1, jBorder = 0;
 
     ListFrame () {
@@ -29,10 +34,9 @@ class ListFrame extends JFrame {
         this.addMouseListener(
                 new MouseAdapter() {
                     @Override
-                    public void mousePressed(MouseEvent e) {
-                        beforePoint = getMousePosition();
-                        int x = beforePoint.x;
-                        int y = beforePoint.y;
+                    public void mousePressed(MouseEvent beforeEvt) {
+                        int x = beforeEvt.getX();
+                        int y = beforeEvt.getY();
 
                         focus = null;
                         for (Figure fig: figs) {
@@ -43,10 +47,18 @@ class ListFrame extends JFrame {
                         }
 
                         if (focus != null) {
-                            figs.remove(focus);
-                            figs.add(focus);
-                            distanceX = focus.x - x;
-                            distanceY = focus.y - y;
+                            boolean canResize = rectResize.x <= x && x <= (rectResize.x + rectResize.w) && rectResize.y <= y && y <= (rectResize.y + rectResize.h);
+                            if (canResize && pressedRes) {
+                                distanceX = rectResize.x - x;
+                                distanceY = rectResize.y - y;
+                            }
+                            else {
+                                pressedRes = false;
+                                figs.remove(focus);
+                                figs.add(focus);
+                                distanceX = focus.x - x;
+                                distanceY = focus.y - y;
+                            }
                         }
 
                         repaint();
@@ -57,18 +69,31 @@ class ListFrame extends JFrame {
         this.addMouseMotionListener(
                 new MouseMotionAdapter() {
                     @Override
-                    public void mouseDragged(MouseEvent e) {
-                        afterPoint = getMousePosition();
-                        int newX = afterPoint.x;
-                        int newY = afterPoint.y;
+                    public void mouseDragged(MouseEvent afterEvt) {
+                        int newX = afterEvt.getX();
+                        int newY = afterEvt.getY();
 
-                        if (focus != null) {
+                        if (pressedRes && focus != null) {
+                            newDistanceX = rectResize.x - newX;
+                            newDistanceY = rectResize.y - newY;
+                            changeX = distanceX - newDistanceX;
+                            changeY = distanceY - newDistanceY;
+
                             figs.remove(focus);
-                            focus.x = newX + distanceX;
-                            focus.y = newY + distanceY;
+                            if (changeX + focus.w >= 15 && changeY + focus.h >= 15) {
+                                focus.resize(changeX, changeY);
+                            }
                             figs.add(focus);
-                            repaint();
                         }
+
+                        else if (!pressedRes && focus != null) {
+                            pressedRes = false;
+                            figs.remove(focus);
+                            focus.drag(newX + distanceX - focus.x, newY + distanceY - focus.y);
+                            figs.add(focus);
+                        }
+
+                        repaint();
                     }
                 }
         );
@@ -77,14 +102,20 @@ class ListFrame extends JFrame {
                 new KeyAdapter() {
                     @Override
                     public void keyPressed (KeyEvent evt) {
-                        p = getMousePosition();
-                        int x = p.x;
-                        int y = p.y;
+                        pos = getMousePosition();
+
+                        if (pos == null) {
+                            return;
+                        }
+
+                        int x = pos.x;
+                        int y = pos.y;
                         int w = 50;
                         int h = 50;
 
                         int letter = evt.getKeyChar();
                         int key = evt.getKeyCode();
+                        pressedRes = false;
 
                         // Creating the figures
                         if (letter == 'r') { // 'r' = create a rectangle
@@ -107,6 +138,22 @@ class ListFrame extends JFrame {
                             focus = figs.get(figs.size() - 1);
                         }
 
+                        // Changing focus
+                        else if (key == 10) { // ENTER = change focus
+                            if (focus == null) {
+                                focus = figs.get(0);
+                            }
+
+                            int index = figs.indexOf(focus) + 1;
+                            if (focus != null) {
+                                if (figs.indexOf(focus) == (figs.size() - 1))
+                                    index = 0;
+                                focus = figs.get(index);
+                                figs.remove(focus);
+                                figs.add(focus);
+                            }
+                        }
+
                         // Changing the colors
                         else if (key == 33 && focus != null) { // PAGE UP = change the filled color
                             if (iFilled >= 10)
@@ -124,39 +171,44 @@ class ListFrame extends JFrame {
 
                         // Moving the figures
                         else if (key == 37 && focus != null) { // LEFT = move the figure to the left
-                            focus.x -= 10;
+                            focus.drag(-10, 0);
                         }
 
                         else if (key == 38 && focus != null) { // UP = move the figure up
-                            focus.y -= 10;
+                            focus.drag(0, -10);
                         }
 
                         else if (key == 39 && focus != null) { // RIGHT = move the figure to the right
-                            focus.x += 10;
+                            focus.drag(10, 0);
                         }
 
                         else if (key == 40 && focus != null) { // DOWN = move the figure down
-                            focus.y += 10;
+                            focus.drag(0, 10);
                         }
 
                         // Changing the figure size
+                        else if (evt.getKeyChar() == '1' && focus != null) { // '1' = resize (mouse)
+                            pressedRes = true;
+                        }
+
+                        else if (evt.getKeyChar() == '2' && focus != null) { // '2' = disable resize (mouse)
+                            pressedRes = false;
+                        }
+
                         else if (evt.getKeyChar() == '-' && focus != null) { // SUBTRACT = reduce the figure size
                             if (focus.w > 10 && focus.h > 10) {
-                                focus.w -= 10;
-                                focus.h -= 10;
+                                focus.resize(-10, -10);
                             }
                         }
 
                         else if (evt.getKeyChar() == '+' && focus != null) { // SUM = increase the figure size
                             if (focus. w != 0 && focus.h != 0) {
-                                focus.w += 10;
-                                focus.h += 10;
+                                focus.resize(10, 10);
                             }
                         }
 
                         else if (key == 61 && focus != null) { // EQUALS = return to the default size
-                            focus.w = 50;
-                            focus.h = 50;
+                            focus.resize(50 - focus.w, 50 - focus.h);
                         }
 
                         // Deleting the figure
@@ -173,6 +225,7 @@ class ListFrame extends JFrame {
 
         this.setTitle("Projeto 1/2 - Editor Grafico Vetorial");
         this.setSize(600, 450);
+        setLocationRelativeTo(null);
     }
 
     public void paint (Graphics g) {
@@ -184,6 +237,11 @@ class ListFrame extends JFrame {
         if (focus != null) {
             rectFocus = new Rect(focus.x-5, focus.y-5, focus.w+10, focus.h+10, null, Color.red);
             rectFocus.redFocus(g);
+            if (pressedRes) {
+                rectResize.x = rectFocus.x + rectFocus.w - 15;
+                rectResize.y = rectFocus.y + rectFocus.h - 15;
+                rectResize.paint(g);
+            }
         }
     }
 }
